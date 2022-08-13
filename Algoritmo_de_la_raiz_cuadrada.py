e65f73f2d468d39c8b6468fd4a0b10ee936133f0
Santiago <santiago.castanomoreno03@gmail.com>
2022-08-13 16:08:19 -0500

    primeros ejercicios metodos numericos

--- Algoritmo_de_la_raiz_cuadrada.py
+++ Algoritmo_de_la_raiz_cuadrada.py
@@ -1,16 +1,27 @@
+from tabulate import tabulate
 txt1="Algoritmo de la raiz cuadrada"
 txt2="ingrese el numero de aproximaciones que desea:"
+
 n=int(input(txt2))
 
 a=5 #este numero corresponde al cual se quiere encontrar su raiz
         #para el ejercicio propuesto es el 5
 x_0=2 #valor inicial propuesto en clase, tambien se propuso 10
           #reemplazar este valor en la variable x_0
-for N in range(n):
+data=[]
 
+for N in range(n):
+    contador=N+1
     x=(1/2)*(x_0+(a/x_0))
+    e=abs(x-x_0)
+    lista=[contador,x,e]
+    data.append(lista)
     if x_0==x:
         break
     x_0=x
-    print(x)
-    
+
+head=["Iteración","Aproximación","Error"]
+print(tabulate(data,headers=head,tablefmt="grid"))
+
+
+
---
+++ README.md
@@ -0,0 +1,3 @@
+Para el algoritmo de la raiz cuadrada se requiere de la libreria "tabulate" para poder ver el resultado tabulado.
+Si no cuenta con esta libreria debe abrir el CMD y escribir lo siguiente:
+pip install tabulate
